7344ab57b9418459f79ce369e9999d126320c942
Jacob O'Bryant <jacobobryant7@gmail.com>
2018-08-11 16:29:24 -0600

    hey

---
+++ journal.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#set -x
+
+year=$(date +%Y)
+month=$(date +%m)
+day=$(date +%d)
+dir=$year/$month/$day
+mkdir -p $dir
+
+function edit {
+    touch $dir/0.md
+    nvim $(echo $dir/* | awk '{print $NF}')
+}
+
+function new {
+    i=0
+    while [ -f $dir/$i.md ]; do
+        ((i++))
+    done
+    touch $dir/$i.md
+    nvim $dir/$i.md
+}
+
+cmd=$1
+if [ "$cmd" = "" ]; then
+    cmd=edit
+fi
+
+$cmd
